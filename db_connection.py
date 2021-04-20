e3aee19f434b8158acdd08cc054bc37e53accc2e
Rojas Carrasco <mrojasc@arpl.com>
2021-04-19 23:19:05 -0500

    first commit

---
+++ db_connection.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(host = "mysql-28407-0.cloudclusters.net", user="admin02", passwd="alayza2213", port="28416")
+cursor = mydb.cursor()
+
+query = ("select * from users;")
+query_1 = ("use genesisDB;")
+
+try:
+	cursor.execute(query_1)
+	cursor.execute(query)
+
+	for (param1, param2, param3, param4) in cursor:
+		print("codigo:{}, nombre:{}, contraseña:{}, level:{}.".format(type(param1), type(param2), type(param3), type(param4)))
+
+	cursor.close()
+	mydb.close()
+except:
+	print("An exception ocurred")
+	cursor.close(
+	mydb.close()
